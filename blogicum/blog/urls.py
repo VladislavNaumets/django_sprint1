30c6cdf7395e10f1059f25b480bbd6d85ed94c0e
Владислав Наумец <klinicpanik@yandex.ru>
2024-08-09 06:00:55 +0300

    pep peeep

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path # type: ignore
+from django.urls import path  # type: ignore
 
 from .views import index, post_detail, category_posts
 
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render # type: ignore
+from django.shortcuts import render  # type: ignore
 
 posts = [
     {
