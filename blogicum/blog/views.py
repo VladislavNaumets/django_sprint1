550a0d3e3ec1149e210bd785b43e15e8252e2e13
Владислав Наумец <klinicpanik@yandex.ru>
2024-08-09 05:27:43 +0300

    sovsem done

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -45,13 +45,13 @@ posts = [
 
 
 def index(request):
-    return render(request, 'blog/includes/index.html', context = {'posts': posts})
+    return render(request, 'blog/index.html', context = {'posts': posts})
 
 
 def post_detail(request, pk):
     post = posts[pk]
-    return render(request, 'blog/includes/detail.html', context = {'post': post})
+    return render(request, 'blog/detail.html', context = {'post': post})
 
 
 def category_posts(request, category_slug):
-    return render(request, 'blog/includes/category.html', {'category_slug': category_slug})
+    return render(request, 'blog/category.html', {'category_slug': category_slug})
